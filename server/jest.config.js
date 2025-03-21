1b92ba813b6f47cb117d1fee91632ebe1459a2d6
Kibaek Kim <kkim@nsls.org>
2025-03-21 16:48:00 -0400

    Revert "Updated"
    This reverts commit f35f62c2fc5c576aee05b248bf27eaf07639e4c5.

--- server/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  clearMocks: true,
-  testPathIgnorePatterns: ['/node_modules/', '/build/'],
-  coverageDirectory: 'coverage',
-  preset: 'ts-jest'
-};
--- server/src/db/config/secret.ts
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-export default {
-  authSecret: '4mFr5Pv2G-;:D{A#',
-  tokenExpireIn: process.env.NODE_ENV == 'development' ? '24h' : '30m',
-  refreshTokenExpireIn: process.env.NODE_ENV == 'development' ? '750h' : '24h'
-};
