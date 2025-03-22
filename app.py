5ed88da0ebf07a6718691b167cb184dc1b25cf9d
ardenhaggin <ahaggin@gmail.com>
2025-03-22 09:47:36 -0700

    Add search functionality to home page and update index and app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 DEVELOPMENT_ENV  = True
 
@@ -11,14 +11,26 @@ app_data = {
     "name":         "Template for a Flask Web App",
     "description":  "A basic Flask app using bootstrap for layout",
     "html_title":   "Template for a Flask Web App",
-    "project_name": "Starter Template",
+    "project_name": "ReviewChew",
     "keywords":     "flask, webapp, template, basic"
 }
 
+# Placeholder function for database search (empty for now)
+def search_database(query):
+    # This is where you will connect to the database in the future
+    # For now, it's empty
+    return []
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', app_data=app_data)
+    query = ''
+    results = []  # This will hold search results after query processing
+    if request.method == 'POST':
+        query = request.form['search_query']  # Get search query from form
+        # Later, you would connect to the database and fetch results here
+        results = search_database(query)
+
+    return render_template('index.html', app_data=app_data, query=query, results=results)
 
 
 @app.route('/about')
