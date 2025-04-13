5583e6ad38577858f196583462d2568c4e9b2dae
ardenhaggin <ahaggin@gmail.com>
2025-04-12 20:45:33 -0400

    added /login, needs aesthetic updates

--- app-flask/app.py
+++ app-flask/app.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import requests
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 DEVELOPMENT_ENV = True
 
 app = Flask(__name__)
+
+app.secret_key = 'dev' # placeholder for now 
 
 app_data = {
     "name": "Template for a Flask Web App",
@@ -52,9 +54,11 @@ def search_database(keywords, stars):
 # Flask route handling
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
     keywords = ''
     stars = ''
     results = []
+    username = session.get('username') 
 
     app_data.update({
         'description': 'Let ReviewChew help you find the best restaurant reviews!'
@@ -72,7 +76,7 @@ def index():
 
     if keywords and stars:
         results = search_database(keywords, stars)  
-    return render_template('index.html', keywords=keywords, results=results, stars=stars, app_data=app_data)
+    return render_template('index.html', keywords=keywords, results=results, stars=stars, app_data=app_data, username=username)
 
 # Fetch the restaurant from the database by its ID (or UUID)
 
@@ -95,6 +99,22 @@ def restaurant_details(restaurant_uuid):
     
     # Handle case if the restaurant is not found or API request fails
     return "Restaurant not found", 404
+
+# Login route 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+
+        # Store the username in the session
+        session['logged_in'] = True
+        session['username'] = username
+
+        # Redirect to the homepage
+        return redirect('/')
+
+    return render_template('login.html', app_data=app_data)
 
 @app.route('/map')
 def map():
